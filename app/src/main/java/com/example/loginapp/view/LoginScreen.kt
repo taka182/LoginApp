4b00eef392cdd5c68cca83d6ae929ee8a6222c52
[taka182] <[soutakaoka92@gmail.com]>
2024-06-30 11:00:46 +0900

    add: Login画面とHome画面を追加

---
+++ app/src/main/java/com/example/loginapp/view/LoginScreen.kt
@@ -0,0 +1,12 @@
+package com.example.loginapp.view
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+
+@Composable
+fun LoginScreen() {
+
+}
