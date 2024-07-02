b3ff61a8823e3df6e758d2b43b7e9816e3367964
[taka182] <[soutakaoka92@gmail.com]>
2024-07-02 23:48:21 +0900

    update: SignInScreenのレイアウトを修正

--- app/src/main/java/com/example/loginapp/view/SignInScreen.kt
+++ app/src/main/java/com/example/loginapp/view/SignInScreen.kt
@@ -7,19 +7,27 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.loginapp.R
 import com.example.loginapp.viewmodels.SignInViewModel
@@ -50,7 +58,8 @@ fun SignInScreen(
             Text(
                 text = emailErrorMessage,
                 color = MaterialTheme.colorScheme.error,
-                style = MaterialTheme.typography.bodySmall
+                style = MaterialTheme.typography.bodySmall,
+                modifier = Modifier.align(Alignment.Start)
             )
             Spacer(modifier = Modifier.height(8.dp))
         }
@@ -62,7 +71,8 @@ fun SignInScreen(
             },
             label = { Text(text = stringResource(R.string.email_label_text)) },
             isError = isEmailError,
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next)
         )
 
         Spacer(modifier = Modifier.height(8.dp))
@@ -71,7 +81,8 @@ fun SignInScreen(
             Text(
                 text = passwordErrorMessage,
                 color = MaterialTheme.colorScheme.error,
-                style = MaterialTheme.typography.bodySmall
+                style = MaterialTheme.typography.bodySmall,
+                modifier = Modifier.align(Alignment.Start)
             )
             Spacer(modifier = Modifier.height(8.dp))
         }
@@ -84,29 +95,49 @@ fun SignInScreen(
             label = { Text(text = stringResource(R.string.password_label_text)) },
             isError = isPasswordError,
             visualTransformation = PasswordVisualTransformation(),
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done)
         )
 
         Spacer(modifier = Modifier.height(8.dp))
+
+        Divider(
+            color = Color.Gray,
+            thickness = 1.dp,
+            modifier = Modifier.padding(vertical = 16.dp)
+        )
 
         Button(
             onClick = {
                 if (signInViewModel.validateEmpty()) {
                     onSignInClick(email, password)
                 }
-            }, modifier = Modifier.fillMaxWidth()
+            },
+            modifier = Modifier.fillMaxWidth()
         ) {
-            Text(text = stringResource(R.string.sign_in))
+            Text(
+                text = stringResource(R.string.sign_in),
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            )
         }
 
-        Button(
+        TextButton(
             onClick = {
                 if (signInViewModel.validateEmpty()) {
                     onSignUpClick(email, password)
                 }
-            }, modifier = Modifier.fillMaxWidth()
+            },
+            modifier = Modifier.fillMaxWidth()
         ) {
-            Text(text = stringResource(R.string.sign_up))
+            Text(
+                text = stringResource(R.string.sign_up),
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colorScheme.primary,
+                textAlign = TextAlign.Center
+            )
         }
     }
 }
